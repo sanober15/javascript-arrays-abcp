ecdb7cd52cb03ef74b48a62d8bb879f700841532
sanober riaz <sanober@sanober.me>
2018-01-15 00:30:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,19 @@
-let chocolateBars = [];
-chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
 function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, elemnt];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
   return array;
 }
