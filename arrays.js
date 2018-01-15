de00990ecf8aaa7c9f13e9e54faf4f8b499d281f
sanober riaz <sanober@sanober.me>
2018-01-15 00:35:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,21 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.unshift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(-1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
