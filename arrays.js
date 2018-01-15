fe52e41da1a0a90207519b2fc791e3ef73dd5f43
sanober riaz <sanober@sanober.me>
2018-01-15 00:27:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,4 +4,7 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element)
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
